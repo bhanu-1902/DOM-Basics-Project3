fc71ad9a9bf546164a71ff21292454ddc7ec02f6
Harshit Bhanushali <harshit.21810372@viit.ac.in>
2021-11-14 09:27:18 +0530

    Define starting conditions

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+//Selecting elements
+const score0 = document.querySelector('#score--0');
+const score1 = document.getElementById('score--1');
+const diceImage = document.querySelector('.dice');
+
+//Starting Conditions
+score0.textContent = 0;
+score1.textContent = 0;
+diceImage.classList.add('hidden');
